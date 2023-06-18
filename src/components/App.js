489e297d430117ae2f6d2a69b9eed3bb11086f4e
Mahebub Maniyar <69462227+sohel53@users.noreply.github.com>
2023-06-18 07:02:27 +0000

    third try

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [count, setCount] = useState("")
+  let [count, setCount] = useState(0)
 
   function incrCount() {
     setCount(count+1);
