b119630102c30be59dbf950f4485357c1459edc8
Mahebub Maniyar <69462227+sohel53@users.noreply.github.com>
2023-06-18 06:58:39 +0000

    secound try

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,15 @@ import './../styles/App.css';
 const App = () => {
   let [count, setCount] = useState("")
 
+  function incrCount() {
+    setCount(count+1);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
         <p>Button Clicked {count} times</p>
+        <button onClick={incrCount}>Click me</button>
 
     </div>
   )
